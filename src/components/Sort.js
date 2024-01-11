8bba0c29ba505ec49a9a76a9cbe7d48e0a40c707
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-01-10 20:16:45 -0500

    Made function to handle sort

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Sort({displayListings, setDisplayListings}) {
     function handleSort() {
+        setDisplayListings(displayListings.sort((a, b) => {
+            const locationA = a.location.toLowerCase()
+            const locationB = b.location.toLowerCase()
 
+            if (locationA < locationB) {
+                return -1
+            }
+            if (locationA > locationB) {
+                return 1
+            }
+            return 0
+        }))
     }
     
     return(
